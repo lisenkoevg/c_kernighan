a7f5bac59afa24db85e440493ee36e058e1c67a4
Evgeny Lysenko <lisenkoevg@gmail.com>
2024-01-07 21:48:46 +0300

    longest_line: correct 0-string terminator

--- 1_9/longest_line.c
+++ 1_9/longest_line.c
@@ -4,13 +4,14 @@
 // 
 
 #include <stdio.h>
-#define MAX_LINE_LENGTH 4
+#define MAX_LINE_LENGTH 40
 
 int getline_(char line[], int lim);
 void copy_(char from[], char to[]);
 void print_array_hex(char arr[], int size);
 
 int main() {
+  printf("MAX_LINE_LENGTH: %d\n", MAX_LINE_LENGTH);
   char line[MAX_LINE_LENGTH + 1];
   char maxline[MAX_LINE_LENGTH + 1];
   int length, maxlength, maxindex;
@@ -18,13 +19,14 @@ int main() {
   int counter = 1;
   int acc = 0;
   while ((length = getline_(line, MAX_LINE_LENGTH)) > 0) {
-//     printf("%3d | ", counter);
-//     if (length == MAX_LINE_LENGTH && line[MAX_LINE_LENGTH - 1] != '\n') {
-//       acc += length;
-//       line[MAX_LINE_LENGTH] = '\0';
-//       printf("%s", line);
-//       continue;
-//     }
+    if (acc == 0)
+      printf("%3d | ", counter);
+    if (length == MAX_LINE_LENGTH && line[MAX_LINE_LENGTH - 1] != '\n') {
+      acc += length;
+      line[MAX_LINE_LENGTH] = '\0';
+      printf("%s", line);
+      continue;
+    }
     if (acc > 0) {
       length += acc;
       acc = 0;
@@ -34,14 +36,11 @@ int main() {
       copy_(line, maxline);
       maxindex = counter;
     }
-//     printf("%s\n", line);
+    printf("%s", line);
     counter++;
   }
   if (maxlength > 0) {
-    printf("\n%3d | ", maxindex);
-    printf("'%s'", maxline);
-    printf("\n%d\n", maxlength);
-    print_array_hex(maxline, MAX_LINE_LENGTH);
+    printf("Maxlength: %d, line %d\n", maxlength, maxindex);
   }
   return 0;
 }
@@ -49,12 +48,12 @@ int main() {
 int getline_(char *line, int lim) {
   char c;
   int i;
-  for (i = 0; (i < lim) && ((c = getchar()) != EOF) && (c != '\n'); i++) {
+  for (i = 0; (i < lim) && ((c = getchar()) != EOF) && c != '\n'; i++) {
     line[i] = c;
   }
-//   if (c == '\n')
-//     line[i] = '^';
-  line[i + 1] = '\0';
+  if (c == '\n')
+    line[i++] = '\n';
+  line[i] = '\0';
   return i;
 }
 
@@ -65,7 +64,7 @@ void copy_(char from[], char to[]) {
 }
 
 void print_array_hex(char arr[], int size) {
-  for (int i = 0; i < size; i++)
+  for (int i = 0; i <= size; i++)
     printf("%#04x ", arr[i]);
   printf("\n");
 }
