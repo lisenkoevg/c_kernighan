54755c8138b9216068fd1ce9cbd271db8056c9c8
Evgeny Lysenko <lisenkoevg@gmail.com>
2024-01-11 01:30:39 +0300

    detab: complete

--- 1_10/detab.c
+++ 1_10/detab.c
@@ -4,7 +4,31 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "../common/lib.c"
+#define TABSIZE 7
 
-int main() {
+int main(int argc, char **argv) {
+  char c, i;
+  unsigned char tabsize = TABSIZE;
+  if (argc > 1) {
+    tabsize = atoi((const char *)argv[1]);
+    assert (tabsize > 0);
+  }
+  printf("tabsize: %u\n", tabsize);
+  while((c = getchar()) != EOF) {
+    if (c == '\t') {
+      do {
+        putchar(' ');
+      } while (++i % tabsize != 0);
+    } else {
+      putchar(c);
+      if (c != '\n')
+        i++;
+      else
+        i = 0;
+    }
+  }
   return 0;
 }
